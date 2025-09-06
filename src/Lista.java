922f1cb8c9a35529c85d5c6d6b20176dc479e894
ronal-4587 <racarvajalinov@ufpso.edu.co>
2025-09-05 21:26:16 -0500

    Add files via upload

---
+++ src/Lista.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Lista {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> carros = new ArrayList<>();
+
+        System.out.print("¿Cuántas frurtas quiere decir? ");
+        int n = sc.nextInt();
+        sc.nextLine(); 
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite el nombre de la fruta " + (i + 1) + ": ");
+            String carro = sc.nextLine();
+            carros.add(carro); 
+        }
+
+        System.out.println("\nLas frutas que ingreso son:");
+        for (int i = 0; i < carros.size(); i++) {
+            System.out.println((i + 1) + ". " + carros.get(i));
+        }
+
+        sc.close();
+    }
+}
